5da65a653f31ba1308ad623019ae0fc2bb3d75ae
leoyakubov <leo.yakubov@gmail.com>
2024-03-01 17:38:03 +0700

    added models

---
+++ auth-server/src/main/java/me/leoyakubov/authserver/model/Role.java
@@ -0,0 +1,17 @@
+package me.leoyakubov.authserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    public final static Role USER = new Role("USER");
+    public final static Role FACEBOOK_USER = new Role("FACEBOOK_USER");
+
+    private String name;
+}
+
